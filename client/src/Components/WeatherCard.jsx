e375c0db906a2505a22bb9255c2557111f19438a
sanjay jat <sanjayjatsanjay22@gmail.com>
2023-07-31 22:48:23 +0530

    pass weatherdata json as a prop to ui component

--- client/src/Components/WeatherCard.jsx
+++ client/src/Components/WeatherCard.jsx
@@ -5,7 +5,12 @@ import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import CompressOutlinedIcon from "@mui/icons-material/CompressOutlined";
 import AirOutlinedIcon from "@mui/icons-material/AirOutlined";
 import CloudOutlinedIcon from "@mui/icons-material/CloudOutlined";
-const WeatherCard = ({ city, temp, pressure, humidity, wind }) => {
+const WeatherCard = ({ weatherData }) => {
+  console.log(weatherData);
+  // console.log(weatherData.weather[0].main);
+  // console.log(weatherData.main.temp);
+  // console.log(weatherData.wind.speed);
+
   return (
     <>
       <div className="weather-cloud">
@@ -15,22 +20,22 @@ const WeatherCard = ({ city, temp, pressure, humidity, wind }) => {
       <div className="weather-city">
         <h1> {"Indore"} </h1>
       </div>
-      <div class="weather-row">
-        <div class="weather-column">
+      <div className="weather-row">
+        <div className="weather-column">
           <LightModeOutlinedIcon />
           <p>Temp: {(500 - 273.15).toFixed(2)} "C</p>
         </div>
-        <div class="weather-column">
+        <div className="weather-column">
           <CompressOutlinedIcon />
           <p>Pressure: {"high"}</p>
         </div>
       </div>
-      <div class="weather-row">
-        <div class="weather-column">
+      <div className="weather-row">
+        <div className="weather-column">
           <CloudOutlinedIcon />
           <p>Humidity: {"humid"}</p>
         </div>
-        <div class="weather-column">
+        <div className="weather-column">
           <AirOutlinedIcon />
           <p>Wind: {1.22}</p>
         </div>
