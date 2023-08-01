a327a0825a083a3849c558ef12a75257bd8138c6
sanjay jat <sanjayjatsanjay22@gmail.com>
2023-08-01 08:36:59 +0530

    custom react hook to fetch api using axios

--- client/src/services/WeatherService.js
+++ client/src/services/WeatherService.js
@@ -1,24 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-const WeatherService = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+const WeatherService = (url) => {
   const [data, setData] = useState(null);
 
-  async function fetchData(city) {
-    try {
-      const response = await axios.get(`http://localhost:8000/${city}`);
-      setData(response.data);
-    } catch (err) {
-      const errorMessage = "Error: " + err.message;
-      setError(errorMessage);
-      console.log(errorMessage);
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setData({
+          ...data,
+          temp: res.data.main.temp,
+          name: res.data.name,
+          humidity: res.data.main.humidity,
+          pressure: res.data.main.pressure,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [url]);
 
-  return { data, fetchData };
+  return { data };
 };
 
 export default WeatherService;
